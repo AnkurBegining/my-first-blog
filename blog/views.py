d7df2d3887d91c2feb03fa985d3aa112d3684aba
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2017-07-29 22:57:34 +0530

    fix: change in view

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
-def post_list(request,pk):
-    posts = get_object_or_404(Post, pk=pk)
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
