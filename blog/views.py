25452d7192e74f77886656f6d9a303af9b7e34d5
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2017-07-30 20:27:48 +0530

    fix: login requirement in edit and add option

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 
-from django.shortcuts import render, get_object_or_404 ,redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -10,12 +10,12 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-
-
+@login_required()
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -29,8 +29,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
-
-
+@login_required()
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -46,22 +45,21 @@ def post_edit(request, pk):
 
 
 
+@login_required()
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 
-
+@login_required()
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
 
 
-
+@login_required()
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
-
-
