bd9c52a34d59234de216c82968231d6867efb229
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2017-07-30 16:32:21 +0530

    fix: log in and log out option added with change in setting. 2

--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,7 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
-@login_required
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -30,7 +30,7 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
-@login_required
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -45,20 +45,20 @@ def post_edit(request, pk):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
-@login_required
+
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 
-@login_required
+
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
 
 
-@login_required
+
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
