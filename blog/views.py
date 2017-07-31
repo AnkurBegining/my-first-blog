f15e8c7e8fb0e69f3574f98dc3aa0200b8c51a5f
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2017-07-31 15:30:41 +0530

    fix: signup creation 4

--- blog/views.py
+++ blog/views.py
@@ -8,12 +8,12 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from blog.forms import SignUpForm
 
-
+@login_required()
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-
+@login_required()
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
