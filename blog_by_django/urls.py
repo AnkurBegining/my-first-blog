ff4e8d893d77c497cdcd72784bfc7cb624647c13
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2017-07-31 14:17:00 +0530

    fix: signup creation 1

--- blog_by_django/urls.py
+++ blog_by_django/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views
-from blog_by_django.blog import views as core_views
+from blog import views as core_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login/$', views.login , name='login'),
     url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
-    url(r'^signup/$', core_views.signup, name='signup'),
+    url(r'^accounts/signup/$', core_views.signup, name='signup'),
     url(r'',include('blog.urls')),
 ]
