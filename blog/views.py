e82b93b69dbbc89a2aa8300b74ae485b975fd560
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2017-08-08 23:10:20 +0530

    fix:host email address changed .3

--- blog/views.py
+++ blog/views.py
@@ -21,7 +21,6 @@ from django.contrib.auth.models import User
 import smtplib
 
 
-@login_required()
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
