1a11021f094e7c99260f4a103a93c3ba9f558f8a
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2017-08-25 14:45:17 +0530

    feat: customize admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-
-# Register your models here.
-
 from .models import Post, Comment
 
-admin.site.register(Post)
+class PostmodelAdmin(admin.ModelAdmin):
+    list_display = ['author','title','created_date']
+    list_display_links = ['created_date']
+    list_editable = ['title']
+    list_filter = ['created_date','title']
+    search_fields = ['title','text']
+
+    class Meta:
+        model=Post
+
+
+admin.site.register(Post, PostmodelAdmin)
 admin.site.register(Comment)
